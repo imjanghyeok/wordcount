38c2fbdd8e21318c19caa7d3aedd6694749a1e49
imjanghyeok <qkdrhkgn23@naver.com>
2023-03-27 02:40:22 +0900

    word count

--- wordcount/main/views.py
+++ wordcount/main/views.py
@@ -9,8 +9,13 @@ def about(request):
     return render(request,'about.html')    
 
 def result(request):
-    if request.method == 'GET': 
-        text = request.GET['text']
+    
+    sequence=request.GET.get('sequence')
+    
+    if not sequence: 
+        text = request.GET.get('text')
+        global text_dict
+        global text_list
         text_list = text.split()
         text_dict = {}
         for word in text_list:
@@ -19,50 +24,19 @@ def result(request):
             else:
                 text_dict[word] = 1
         return render(request,'result.html',{'words':text_dict.items()})
-    elif request.POST['sequence'] == "ascending_key": 
-        text = request.GET['text']
-        text_list = text.split()
-        text_dict = {}
-        for word in text_list:
-            if word in text_dict:
-                text_dict[word] +=1
-            else:
-                text_dict[word] = 1
-        words = sorted(text_dict.items(), key=lambda x:x[0], reverse=False)
-        return render(request,'result.html',{'words':words})
-    elif request.POST['sequence'] == "descending_key":
-        text = request.GET['text']
-        text_list = text.split()
-        text_dict = {}
-        for word in text_list:
-            if word in text_dict:
-                text_dict[word] +=1
-            else:
-                text_dict[word] = 1
-        words = sorted(text_dict.items(), key=lambda x:x[0], reverse=True)
-        return render(request,'result.html',{'words':words})
-    elif request.POST['sequence'] == "ascending_value":
-        text = request.GET['text']
-        text_list = text.split()
-        text_dict = {}
-        for word in text_list:
-            if word in text_dict:
-                text_dict[word] +=1
-            else:
-                text_dict[word] = 1
-        words = sorted(text_dict.items(), key=lambda x:x[1], reverse=False)
-        return render(request,'result.html',{'words':words})
-    elif request.POST['sequence'] == "descending_value":
-        text = request.GET['text']
-        text_list = text.split()
-        text_dict = {}
-        for word in text_list:
-            if word in text_dict:
-                text_dict[word] +=1
-            else:
-                text_dict[word] = 1
-        words = sorted(text_dict.items(), key=lambda x:x[1], reverse=True)
-        return render(request,'result.html',{'words':words})
+    else:
+        if request.GET['sequence'] == "ascending_key": 
+            words = sorted(text_dict.items(), key=lambda x:x[0], reverse=False)
+            return render(request,'result.html',{'words':words})
+        elif request.GET['sequence'] == "descending_key":
+            words = sorted(text_dict.items(), key=lambda x:x[0], reverse=True)
+            return render(request,'result.html',{'words':words})
+        elif request.GET['sequence'] == "ascending_value":
+            words = sorted(text_dict.items(), key=lambda x:x[1], reverse=False)
+            return render(request,'result.html',{'words':words})
+        elif request.GET['sequence'] == "descending_value":
+            words = sorted(text_dict.items(), key=lambda x:x[1], reverse=True)
+            return render(request,'result.html',{'words':words})
     
     
     
